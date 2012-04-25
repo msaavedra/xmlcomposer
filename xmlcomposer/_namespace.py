3403994884a45ac2b6d99c902eed9dc458ac7952
Michael Saavedra <mtsaavedra@gmail.com>
2012-04-25 15:49:26 -0700

    Add doctests and other comments, fix a bug in __contains__ uncovered by doctest.

--- xmlcomposer/_namespace.py
+++ xmlcomposer/_namespace.py
@@ -5,8 +5,47 @@ from types import ModuleType
 
 class Namespace(ModuleType):
     """A module-like object representing an XML namespace.
+    
+    Example:
+    >>> xhtml = Namespace(module='xmlcomposer.formats.xhtml_1_strict')
+    >>> print xhtml
+    <Namespace "http://www.w3.org/1999/xhtml">
+    
+    Membership can be tested:
+    >>> from xmlcomposer.formats import html5
+    >>> html = Namespace(module=html5)
+    >>> html5.Title in html
+    True
+    
+    One can iterate over the Elements in the namespace:
+    >>> from xmlcomposer import Element
+    >>> class Author(Element): pass
+    >>> class Title(Element): pass
+    >>> n = Namespace(elements=(Author, Title))
+    >>> print sorted([element.__name__ for element in n])
+    ['Author', 'Title']
     """
     def __init__(self, name='', prefix='', module=None, elements=()):
+        """Create a new namespace instance
+        
+        The name arg is the globally unique string (used in an xmlns attribute)
+        that identifies the namespace. If not specified, no namespace
+        declaration will be made in generated XML.
+        
+        The prefix arg, if supplied, will cause each element in the namespace
+        to be preceded by the supplied string when generated in the format
+        "prefix:element".
+        
+        The module arg allows one to add the contents of a module to the
+        namespace. The value may be a module instance, or a string (in which
+        case the module is imported automatically). Only subclasses of Element
+        are added to the namespace. If the __namespace__ name is defined in
+        the module, it will be used to set the name attribute of the new
+        Namespace instance.
+        
+        The elements arg is an alternate method of adding elements to the
+        namespace. It should be a sequence of Element subclasses.
+        """
         super(Namespace, self).__init__(name)
         super(Namespace, self).__setattr__('__prefix__', prefix)
         del self.__doc__
@@ -44,6 +83,9 @@ class Namespace(ModuleType):
                 yield obj
     
     def __contains__(self, element):
+        if isinstance(element, str):
+            print [e for e in iter(self)]
+            return element in (e.__class__.__name__ for e in iter(self))
         return (element in iter(self))
     
     def __eq__(self, other):
